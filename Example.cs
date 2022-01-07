753974e575ff1c9de96828fa7385ef910b6e8261
Ramin Rahimzada <raminrahimzada@gmail.com>
2022-01-08 00:30:55 +0400

    add example.cs class

---
+++ Example.cs
@@ -0,0 +1,10 @@
+// Prints hello as red and world as green
+var hello = "Hello";
+var world = "World";
+CConsole.WriteLine($"{hello:red} {world:green}");
+
+
+//Set foreground and background colors also
+var hello = "Hello";
+var world = "World";
+CConsole.WriteLine($"{hello:red:cyan} {world:green:yellow}");
